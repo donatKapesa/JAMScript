a4b7271ebbd88ef3e13c07be4d62fe7946f49f1d
Muthucumaru Maheswaran <mahes25@gmail.com>
2016-06-11 13:16:23 -0400

    Modified chat. Seems to be working.

--- lib/jamlib/c_core/testers/Xiru_JAM_Tests/chat.c
+++ lib/jamlib/c_core/testers/Xiru_JAM_Tests/chat.c
@@ -3,39 +3,18 @@
 #include <stdio.h>
 #include <string.h>
 #include "task.h"
+#include <poll.h>
 
 #define NUM_MSG 20
 
 int ID = 0;
 
-/*
-struct Task
-{
-	char	name[256];	// offset known to acid
-	char	state[256];
-	Task	*next;
-	Task	*prev;
-	Task	*allnext;
-	Task	*allprev;
-	Context	context;
-	uvlong	alarmtime;
-	uint	id;
-	uchar	*stk;
-	uint	stksize;
-	int	exiting;
-	int	alltaskslot;
-	int	system;
-	int	ready;
-	void	(*startfn)(void*);
-	void	*startarg;
-	void	*udata;
-};
-*/
+struct pollfd qpollfds[2];
 
 
 void jam_run_app(void *arg)
 {
-    char * buf = NULL;;
+    char * buf = NULL;
     size_t len = 0;
     taskname("APP");
     printf("Enter your desired username: ");
@@ -54,19 +33,30 @@ void jam_run_app(void *arg)
     free(buf);
     len = 0;
 
+    qpollfds[0].fd = 0;
+    qpollfds[0].events = POLLIN;
+
     while(1){
-      printf("\n\n\n%s: ", usr_name);
-      getline(&buf, &len, stdin);
+        buf = NULL;
+      int numfds = poll(qpollfds, 1, 500);
+
+      if (qpollfds[0].revents & POLLIN)
+        {
+          printf("\n\n\n%s: ", usr_name);            
+          getline(&buf, &len, stdin);
+          printf("Your input: %s\n", buf);
+          jam_rexec_async(js, "j_node_get_msg", "ssi" , usr_name, buf, ID);
+        }
       taskyield();
-      printf("\n\n\n");
-      print_task_queue();
-      jam_rexec_async(js, "j_node_get_msg", "ssi" , usr_name, buf, ID);
       len = 0;
       free(buf);
-      //taskdelay(2000);
+      taskyield();
+      //taskdelay(200);
     }
 
 }
+
+
 
 void c_node_get_msg(char * usr_name, char * msg, int msg_id){
     printf("\n----------------You have received a Message-------------------\n");
@@ -89,14 +79,12 @@ void taskmain(int argc, char **argv)
 {
     taskname("mainprogram");
     jamstate_t *js = jam_init();
-    print_task_queue();
     // Start the event loop in another thread.. with cooperative threads.. we
     // to yield for that thread to start running
     taskcreate(jam_event_loop, js, STACKSIZE);
 
     // create the application runner
     taskcreate(jam_run_app, js, STACKSIZE);
-    print_task_queue();
     activity_regcallback(js->atable, "c_node_get_msg", ASYNC, "ssi", callc_node_get_msg);
 
     printf("Commencing JAM operation \n");
