98f966cd550635bef2e350b1ae2b48d0be06f820
FandiYi <fandi.yi@mail.mcgill.ca>
2019-11-28 23:38:37 -0500

    add test for C2J localandj.jxe

--- tests/C2J/localandj.js
+++ tests/C2J/localandj.js
@@ -1,10 +1,15 @@
+var expect = require('chai').expect
 
 jasync function me(s) {
     console.log("Me - Message from C ", s);
+    expect(s).to.equal('cxxxxyyyy');
+    console.log("================passed==============");
 }
 
 jasync function you(s) {
     console.log("You - Message from C ", s);
+    expect(s).to.equal("cxxxxxxxx");
+    console.log("================passed==============");
 }
 
 
