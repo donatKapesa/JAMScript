52195648350b1fa99c2617c12fd791b65350c943
Volen <volen.mihaylov@yahoo.com>
2020-02-06 10:19:49 -0500

    J2C tests added

--- tests/J2C-Chai/SYNC/cld-j2c.js
+++ tests/J2C-Chai/SYNC/cld-j2c.js
@@ -1,7 +1,9 @@
+var expect = require('chai').expect
 
 setInterval(()=> {
     if (jsys.type === "cloud") {
 	var x = dotask();
+	expect(x.device).to.equal(3);
 	console.log(x);
     }
 }, 1000);
--- tests/J2C-Chai/SYNC/fog-j2c.js
+++ tests/J2C-Chai/SYNC/fog-j2c.js
@@ -3,5 +3,6 @@ setInterval(()=> {
     if (jsys.type === "fog") {
 	var x = dotask();
 	console.log(x);
+	expect(x.device).to.equal(3);
     }
 }, 1000);
