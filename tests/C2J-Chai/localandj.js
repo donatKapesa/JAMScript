e2560809bcfdd7f65413f01eef6c65540c7e2fa4
Volen-M <volen.mihaylov@yahoo.com>
2020-05-01 16:50:36 -0400

    Forgot to commit a month ago from laptop

--- tests/C2J-Chai/localandj.js
+++ tests/C2J-Chai/localandj.js
@@ -3,13 +3,13 @@ var expect = require('chai').expect
 jasync function me(s) {
     console.log("Me - Message from C ", s);
     expect(s).to.equal('cxxxxyyyy');
-    console.log("================passed==============");
+    console.log("================passed-1==============");
 }
 
 jasync function you(s) {
     console.log("You - Message from C ", s);
     expect(s).to.equal("cxxxxxxxx");
-    console.log("================passed==============");
+    console.log("================passed-2==============");
 }
 
 
